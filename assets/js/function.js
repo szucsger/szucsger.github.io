73c00fab649cd50324af79557a797dd480750958
Szücs Gergely <94920854+szucsger@users.noreply.github.com>
2025-11-15 15:23:00 +0100

    Add files via upload

---
+++ assets/js/function.js
@@ -0,0 +1,35 @@
+// smooth scroll
+$(document).ready(function () {
+  $(".navbar .nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        700,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+// navbar toggle
+$("#nav-toggle").click(function () {
+  $(this).toggleClass("is-active");
+  $("ul.nav").toggleClass("show");
+});
+
+// navbar affix on scroll
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 50) {
+    $(".custom-navbar").addClass("affix");
+  } else {
+    $(".custom-navbar").removeClass("affix");
+  }
+});
